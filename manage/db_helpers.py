fb47dcfdd945203e0795c77fa6fa0a75a6798d32
Fabian P. Schmidt <kerel-fs@gmx.de>
2018-08-27 02:32:46 +0200

    Add licenses

--- manage/db_helpers.py
+++ manage/db_helpers.py
@@ -1,3 +1,25 @@
+'''
+db/base/helpers/helpers.py
+source: https://gitlab.com/librespacefoundation/satnogs/satnogs-db
+
+
+SatNOGS DB - SatNOGS DB is a transmitter suggestions and crowd-sourcing app
+Copyright (C) 2014-2018  Libre Space Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWX'
 LOWER = 'abcdefghijklmnopqrstuvwx'
 
