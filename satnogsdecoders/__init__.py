f6d2e9a13d8075c22d76bed2da1fd9c93c243ca0
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-10-23 19:32:33 +0300

    Package 'satnogsdecoders' into a Python module
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

---
+++ satnogsdecoders/__init__.py
@@ -0,0 +1,10 @@
+"""
+SatNOGS Decoders module initialization
+"""
+from __future__ import absolute_import
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+
+del get_versions
---
+++ setup.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup
+import versioneer
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
