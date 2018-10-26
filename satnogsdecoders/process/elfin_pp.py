a05f2061acbe44e60e8ff84e7edd3c1f4e55b6a6
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-10-27 02:33:17 +0300

    Fix linting errors
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- satnogsdecoders/process/elfin_pp.py
+++ satnogsdecoders/process/elfin_pp.py
@@ -1,6 +1,7 @@
-#
-# Preprocessor used in kaitai struct to remove the escape sequences of
-# an ELFIN STAR telemetry frame
+"""
+Preprocessor used in kaitai struct to remove the escape sequences of
+an ELFIN STAR telemetry frame
+"""
 #
 # Patrick Dohmen, DL4PD (dl4pd@darc.de)
 # With the kind help of Mikhail Yakshin
@@ -14,20 +15,31 @@
 #         process: elfin_pp
 #         size-eos: true
 #
+from __future__ import absolute_import, division, print_function
 
 import binascii
 
 
-class ElfinPp:
-    def decode(self, bindata):
+class ElfinPp(object):  # pylint: disable=too-few-public-methods
+    """
+    ELFIN preprocessor class
+    """
+
+    def __init__(self):
+        pass
+
+    def decode(self, bindata):  # pylint: disable=no-self-use
+        """
+        ELFIN preprocessor decoding method
+        """
         i = 0
         binlen = len(bindata)
         out = b''
         while i < binlen:
-            ch = ord(bindata[i])
-            if ch == 0x27 and i + 1 < binlen:
-                next_ch = ord(bindata[i + 1])
-                if next_ch == 0x27 or next_ch == 0x5e or next_ch == 0x9e:
+            char = ord(bindata[i])
+            if char == 0x27 and i + 1 < binlen:
+                next_char = ord(bindata[i + 1])
+                if next_char in [0x27, 0x5e, 0x9e]:
                     i += 1
             out += bindata[i]
             i += 1
@@ -37,13 +49,13 @@ class ElfinPp:
 if __name__ == "__main__":
 
     # some examples
-    dec = ElfinPp()
-    print binascii.hexlify(binascii.unhexlify('0011272722'))
-    print binascii.hexlify(dec.decode(binascii.unhexlify('0011272722')))
+    DEC = ElfinPp()
+    print(binascii.hexlify(binascii.unhexlify('0011272722')))
+    print(binascii.hexlify(DEC.decode(binascii.unhexlify('0011272722'))))
 
-    print binascii.hexlify(binascii.unhexlify('0011a2727220'))
-    print binascii.hexlify(dec.decode(binascii.unhexlify('0011a2727220')))
+    print(binascii.hexlify(binascii.unhexlify('0011a2727220')))
+    print(binascii.hexlify(DEC.decode(binascii.unhexlify('0011a2727220'))))
 
-    print binascii.hexlify(binascii.unhexlify('930027005e0027932727275e'))
-    print binascii.hexlify(
-        dec.decode(binascii.unhexlify('930027005e0027932727275e')))
+    print(binascii.hexlify(binascii.unhexlify('930027005e0027932727275e')))
+    print(binascii.hexlify(
+        DEC.decode(binascii.unhexlify('930027005e0027932727275e'))))
