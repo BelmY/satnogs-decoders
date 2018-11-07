8100cd4df1ab87a6c06c3b4954a4656dd966d06b
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-11-07 16:36:04 +0200

    Make decoder classes available in 'decoder' subpackage namespace
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

---
+++ satnogsdecoders/decoder/__init__.py
@@ -0,0 +1,25 @@
+"""
+SatNOGS Decoder subpackage initialization
+"""
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    'Ax25frames',
+    'Cubebel1',
+    'Elfin',
+    'Fox',
+    'Qbee',
+    'Siriussat',
+    'Skcube',
+    'Strand',
+]
+
+from .ax25frames import Ax25frames
+from .cubebel1 import Cubebel1
+from .elfin import Elfin
+from .fox import Fox
+from .fox import Fox
+from .qbee import Qbee
+from .siriussat import Siriussat
+from .skcube import Skcube
+from .strand import Strand
