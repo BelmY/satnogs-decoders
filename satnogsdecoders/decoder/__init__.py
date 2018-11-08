4ea08f5fdaac51266aabc8e580428d59a1d153a7
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-11-08 12:14:53 +0200

    Add missing decoder classes in package namespace
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- satnogsdecoders/decoder/__init__.py
+++ satnogsdecoders/decoder/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, division, print_function
 
 __all__ = [
     'Ax25frames',
+    'Cas4',
     'Cubebel1',
     'Elfin',
     'Fox',
@@ -12,9 +13,11 @@ __all__ = [
     'Siriussat',
     'Skcube',
     'Strand',
+    'Us6',
 ]
 
 from .ax25frames import Ax25frames
+from .cas4 import Cas4
 from .cubebel1 import Cubebel1
 from .elfin import Elfin
 from .fox import Fox
@@ -22,3 +25,4 @@ from .qbee import Qbee
 from .siriussat import Siriussat
 from .skcube import Skcube
 from .strand import Strand
+from .us6 import Us6
