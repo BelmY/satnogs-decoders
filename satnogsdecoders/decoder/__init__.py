1e6d80955a396a2ad33d70c3512c3980a2103575
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-12-04 19:25:18 +0200

    Introduce function to get fields from docstrings
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- satnogsdecoders/decoder/__init__.py
+++ satnogsdecoders/decoder/__init__.py
@@ -20,6 +20,8 @@ __all__ = [
     'Us6',
 ]
 
+import re
+
 from .ax25frames import Ax25frames
 from .cas4 import Cas4
 from .cubebel1 import Cubebel1
@@ -34,3 +36,28 @@ from .siriussat import Siriussat
 from .skcube import Skcube
 from .strand import Strand
 from .us6 import Us6
+
+FIELD_REGEX = re.compile(
+    r':field (?P<field>[\*\w]+): (?P<attribute>.*?)'
+    r'(?:(?=:field)|\Z|\n)', re.S)
+
+
+def get_fields(struct, empty=False):
+    '''
+    Get fields defined in docstring
+    '''
+    fields = {}
+
+    try:
+        doc_fields = FIELD_REGEX.findall(struct.__doc__)
+    except TypeError:
+        return fields
+
+    for key, value in doc_fields:
+        try:
+            fields[key] = reduce(getattr, value.split('.'), struct)
+        except AttributeError:
+            if empty:
+                fields[key] = None
+
+    return fields
