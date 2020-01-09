1645901bfe146d32ad8be61015a4b74f75d4e1b2
deckbsd <deckbsd@protonmail.com>
2020-01-09 11:49:05 -0800

    fix generate fields issues Signed-off-by: Julien Flawinne <flawinne.julien at protonmail dot com>

--- contrib/generate_field_docstring.py
+++ contrib/generate_field_docstring.py
@@ -2,13 +2,44 @@
 import argparse
 import yaml
 
+fields_found = {}
+exclude_rules = []
+parameters = {
+    'path_depth': 0
+}
+
+# RULES
+def NoRaw(external_identifier):
+    if external_identifier.find("raw"):
+        return False
+
+    return True
 
 def add_docs_field(external_identifier, internal_identifier):
-    print('  :field {0}: {1}'.format(external_identifier, '.'.join(internal_identifier)))
-    pass
+    global fields_found
+    global exclude_rules
 
+    for rule in exclude_rules:
+        if rule(external_identifier):
+            return
 
-def generate_field_docstring(ksy):    
+    if external_identifier in fields_found:
+        if fields_found[external_identifier].sort == internal_identifier.sort:
+        # same path
+            return
+
+        path_depth = parameters['path_depth']
+        # same field name but path is different
+        diff_in_path = [x for x in internal_identifier if x not in fields_found[external_identifier]]
+        path_len = len(diff_in_path)
+        if path_depth != 0 and path_depth < path_len:
+            diff_in_path = diff_in_path[(path_len-path_depth):]
+        new_name = '_'.join(diff_in_path + [external_identifier])
+        fields_found[new_name]=internal_identifier
+    else:
+        fields_found[external_identifier]=internal_identifier
+
+def generate_field_docstring(ksy):
     print('doc: |')
     gen_fields_from_seq(ksy, ksy['seq'], prefix=[])
     if 'instances' in ksy.keys():
@@ -17,7 +48,7 @@ def generate_field_docstring(ksy):
 
 def gen_fields_from_instances(ksy, instances, prefix):
     for instance in instances.keys():
-        add_docs_field('{0}_{1}'.format(ksy['meta']['id'], instance),
+        add_docs_field(instance,
                        prefix + [instance])
 
 
@@ -25,8 +56,8 @@ def gen_fields_from_seq(ksy, seq, prefix):
     for field in seq:
         if not 'type' in field.keys():
             # Field is a raw byte array
-            add_docs_field('{0}_{1}'.format(ksy['meta']['id'], field['id']),
-                           prefix + [field['id']])
+            add_docs_field(field['id'],
+                           prefix + [field['id']], path_depth)
             continue
 
         if type(field['type']) is dict:
@@ -50,7 +81,7 @@ def gen_fields_for_field(ksy, seq, prefix, field, field_type):
                                       prefix + [field['id']])
     else:
         # Field has basic data type
-        add_docs_field('{0}_{1}'.format(ksy['meta']['id'], field['id']),
+        add_docs_field(field['id'],
                        prefix + [field['id']])
 
 
@@ -62,9 +93,20 @@ if __name__ == '__main__':
     parser.add_argument('ksy_file',
                         type=str,
                         help='Kaitai Struct file')
+    parser.add_argument('--no-raw', dest='no_raw', action='store_true',
+                            help='Exclude the fields where the raw word is in the id.')
+    parser.add_argument('--keeping-path-depth', dest='path_depth', default=0,
+                            help='Specify the number of path element to be included in the field name in case of same id.')
     args = parser.parse_args()
 
-    with open(args.ksy_file, "r") as f:
-        ksy = yaml.load(f)
+    if args.no_raw is True:
+        exclude_rules.append(NoRaw)
 
+    with open(args.ksy_file, "r") as f:
+        ksy = yaml.load(f, Loader=yaml.SafeLoader)
+
+    parameters['path_depth'] = int(args.path_depth)
     generate_field_docstring(ksy)
+
+    for key in fields_found:
+        print('\t:field ' + ksy['meta']['id'] + '_' + key + ': ' + '.'.join(fields_found[key]))
